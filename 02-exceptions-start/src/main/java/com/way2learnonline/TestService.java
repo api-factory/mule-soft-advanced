9c71fee858c1962984e6599c21a5ebd2204763ae
api-factory <45937987+api-factory@users.noreply.github.com>
2018-12-20 15:54:54 +0530

    Add files via upload

---
+++ 02-exceptions-start/src/main/java/com/way2learnonline/TestService.java
@@ -0,0 +1,15 @@
+package com.way2learnonline;
+
+public class TestService {
+	
+	public String getMessage(String message){
+		
+		if(true){
+			throw new RuntimeException("Just for Testing!!");
+		}
+		return message;
+		
+		
+	}
+
+}
